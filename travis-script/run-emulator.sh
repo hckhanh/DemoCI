1c6d2389bc5e6ba3f6961c6442096e794340275a
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-14 16:26:13 +0700

    Update with 2 success jobs

---
+++ travis-script/run-emulator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# Run emulator
+chmod -R ugo+x travis-script/
+echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a # x86 or armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+android-wait-for-emulator
+adb shell input keyevent 82 &
