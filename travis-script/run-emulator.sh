e3bf00372c6f524a745d4d52c595ab5eb717ecb6
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-15 00:22:10 +0700

    Update travis script

--- travis-script/run-emulator.sh
+++ travis-script/run-emulator.sh
@@ -4,5 +4,6 @@ set -e
 
 # Run emulator
 echo no | android create avd --force -n test -t ${ANDROID_API} --abi ${ANDROID_TARGET}
+emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
