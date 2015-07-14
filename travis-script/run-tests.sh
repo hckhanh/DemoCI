107d536b39c51983101cba17652e4d8b781df852
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-14 10:50:50 +0700

    Edit some bug

--- travis-script/run-tests.sh
+++ travis-script/run-tests.sh
@@ -4,5 +4,10 @@ set -ev
 
 if [ ! -z "${ANDROID_APP1}" ]; then
 	cd ${ANDROID_DIR}/${ANDROID_APP1_NAME}
-	./gradlew build ANDROID_APP1
+	./gradlew build ${ANDROID_APP1}
+fi
+
+if [ ! -z "${ANDROID_APP2}" ]; then
+	cd ${ANDROID_DIR}/${ANDROID_APP2_NAME}
+	./gradlew build ${ANDROID_APP2}
 fi
