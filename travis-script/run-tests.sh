4ccf679fc573553db8d2822afed43aca8e096b5c
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-14 13:40:40 +0700

    Update .travis.yml

--- travis-script/run-tests.sh
+++ travis-script/run-tests.sh
@@ -4,11 +4,12 @@ set -ev
 
 if [ ! -z "${ANDROID_APP1}" ]; then
 	cd ${ANDROID_DIR}/${ANDROID_APP1_NAME}
-	chmod -R +x
+	chmod -R 755
 	./gradlew build connectedCheck
 fi
 
 if [ ! -z "${ANDROID_APP2}" ]; then
 	cd ${ANDROID_DIR}/${ANDROID_APP2_NAME}
+	
 	./gradlew build ${ANDROID_APP2}
 fi
