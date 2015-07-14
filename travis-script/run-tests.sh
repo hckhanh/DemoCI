ee72a46b801e1f49948313db637129db1226e6e0
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-14 16:04:00 +0700

    Update with 1 success job

--- travis-script/run-tests.sh
+++ travis-script/run-tests.sh
@@ -4,11 +4,10 @@ set -ev
 
 if [ ! -z "${ANDROID_APP1}" ]; then
 	cd ${ANDROID_DIR}/${ANDROID_APP1_NAME}
-	./gradlew build connectedCheck
+	./gradlew build ${ANDROID_APP1}
 fi
 
 if [ ! -z "${ANDROID_APP2}" ]; then
 	cd ${ANDROID_DIR}/${ANDROID_APP2_NAME}
-	adb devices
 	./gradlew build ${ANDROID_APP2}
 fi
