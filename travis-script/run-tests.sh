771f6f23f37c91542e0671c50bcba820e3dacd11
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-14 10:53:52 +0700

    Update .travis.yml

--- travis-script/run-tests.sh
+++ travis-script/run-tests.sh
@@ -4,6 +4,7 @@ set -ev
 
 if [ ! -z "${ANDROID_APP1}" ]; then
 	cd ${ANDROID_DIR}/${ANDROID_APP1_NAME}
+	echo "Run test \$ANDROID_APP1: '$ANDROID_APP1' ${ANDROID_APP1}"
 	./gradlew build ${ANDROID_APP1}
 fi
 
