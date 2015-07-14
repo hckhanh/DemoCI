bc9331da133723d6be11e614fc136825a6cae3ba
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-14 19:46:15 +0700

    Add ANDROID_API and ANDROID_TARGET env

--- travis-script/run-emulator.sh
+++ travis-script/run-emulator.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Run emulator
-echo no | android create avd --force -n test -t android-22 --abi x86 # x86 or armeabi-v7a
+echo no | android create avd --force -n test -t ${ANDROID_API} --abi ${ANDROID_TARGET} # x86 or armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
