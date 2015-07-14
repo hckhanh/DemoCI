8f163f89f7ed3ef46b88195e992f7c00c002d681
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-14 13:20:44 +0700

    Edit some bug

--- travis-script/run-tests.sh
+++ travis-script/run-tests.sh
@@ -4,7 +4,7 @@ set -ev
 
 if [ ! -z "${ANDROID_APP1}" ]; then
 	cd ${ANDROID_DIR}/${ANDROID_APP1_NAME}
-	./gradlew build ${ANDROID_APP1}
+	./gradlew build connectedCheck
 fi
 
 if [ ! -z "${ANDROID_APP2}" ]; then
