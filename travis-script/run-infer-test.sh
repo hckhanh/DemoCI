efbb6a38869ab7cde6bc073af452fc3ca57bd245
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-15 11:01:28 +0700

    Devide to multiple files

---
+++ travis-script/run-infer-test.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+
+set -e
+
+# Run infer test
+cd ..
+cd Client/Client.Android/DemoApp1/
+infer -- ./gradlew build
+cd ../DemoApp2/
+infer -- ./gradlew build
+cd ../../../
--- travis-script/run-test-cases.sh
+++ travis-script/run-test-cases.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Run Android emulator
+./travis-script/run-emulator.sh
+
 if [ ! -z "${ANDROID_APP1}" ]; then
 	cd ${ANDROID_DIR}/${ANDROID_APP1_NAME}
 	./gradlew build ${ANDROID_APP1}
