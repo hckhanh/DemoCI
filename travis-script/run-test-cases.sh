02b67b5cf822f235d7555a4dabf8727e264e5c45
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-16 09:46:48 +0700

    Fix run-test-cases.sh file

--- travis-script/run-test-cases.sh
+++ travis-script/run-test-cases.sh
@@ -5,7 +5,7 @@ set -ev
 # Run Android emulator
 if [ ! -z "${ANDROID_APP1}" ] || [ ! -z "${ANDROID_APP2}" ]; then
 	# Run infer test
-	./travis-script/run-infer-test.
+	./travis-script/run-infer-test.sh
 
 	# Run android emulator
 	./travis-script/run-emulator.sh
