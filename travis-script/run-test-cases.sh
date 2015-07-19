8f9717fa1cbd7cfda6bc705eb8c4a9ee6db6e07d
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-19 23:09:56 +0700

    Edit run-test-cases.sh (6)

--- travis-script/run-test-cases.sh
+++ travis-script/run-test-cases.sh
@@ -13,13 +13,13 @@ if [ ! -z "${ANDROID_APP1}" ] || [ ! -z "${ANDROID_APP2}" ]; then
 	# This is not correct, yet.
 	if [ ! -z "${ANDROID_APP1}" ]; then
 		cd $ANDROID_DIR/$ANDROID_APP1_NAME/
-		./gradlew $ANDROID_APP1
+		./gradlew build $ANDROID_APP1
 		cd $TRAVIS_BUILD_DIR/
 	fi
 
 	if [ ! -z "${ANDROID_APP2}" ]; then
 		cd $ANDROID_DIR/$ANDROID_APP2_NAME/
-		./gradlew $ANDROID_APP2
+		./gradlew build $ANDROID_APP2
 		cd $TRAVIS_BUILD_DIR/
 	fi
 fi
