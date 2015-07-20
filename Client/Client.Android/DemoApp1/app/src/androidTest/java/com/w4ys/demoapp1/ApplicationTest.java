90d78b7874cf22795422e3440b023b33beb0c29f
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-20 10:58:05 +0700

    edit something

--- Client/Client.Android/DemoApp1/app/src/androidTest/java/com/w4ys/demoapp1/ApplicationTest.java
+++ Client/Client.Android/DemoApp1/app/src/androidTest/java/com/w4ys/demoapp1/ApplicationTest.java
@@ -1,8 +1,13 @@
 package com.w4ys.demoapp1;
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.action.ViewActions;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -14,25 +19,31 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
 public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     public ApplicationTest() {
         super(MainActivity.class);
     }
 
-    @Override
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         getActivity();
     }
 
+    @Test
+    public void testConstValue() {
+        assertEquals(1, 2);
+    }
+
+    @Test
     public void testSum2PositiveNumber() {
         int number1 = 12;
         int number2 = 10;
         int expectedResult = 22;
-
-        ViewActions.closeSoftKeyboard();
 
         onView(withId(R.id.editTextNumber1)).perform(typeText(Integer.toString(number1)));
         onView(withId(R.id.editTextNumber2)).perform(typeText(Integer.toString(number2)));
--- Client/Client.Android/DemoApp2/app/src/main/java/com/w4ys/demoapp2/MainActivity.java
+++ Client/Client.Android/DemoApp2/app/src/main/java/com/w4ys/demoapp2/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Clicked!");
+            }
+        });
     }
 
     @Override
