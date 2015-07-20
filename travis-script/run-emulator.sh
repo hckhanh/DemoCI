e310b26d599c3c118e112cb14f6b8fb9f0ba5479
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-20 07:44:38 +0700

    Edit travis.yml

--- travis-script/run-emulator.sh
+++ travis-script/run-emulator.sh
@@ -3,7 +3,7 @@
 set -ev
 
 # Run emulator
-echo no | android create avd --force -n test -t $ANDROID_API --abi $ANDROID_TARGET
+echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
