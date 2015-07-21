7ed21a6dbbb243003a425b24a2fd52176228e6c0
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-21 19:24:58 +0700

    Update bash scripts

--- travis-script/run-infer-test.sh
+++ travis-script/run-infer-test.sh
@@ -4,7 +4,9 @@ set -ev
 
 # Run infer test
 cd $ANDROID_DIR/$ANDROID_APP1_NAME/
+chmod -R ugo+x ./gradlew
 infer -- ./gradlew build
 cd ../$ANDROID_APP2_NAME/
+chmod -R ugo+x ./gradlew
 infer -- ./gradlew build
 cd $TRAVIS_BUILD_DIR/
