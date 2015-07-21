2df6f30083045084ff512581ef3a0722b55395ac
hckhanh <hoangchaukhanh93@gmail.com>
2015-07-21 19:34:29 +0700

    Update bash scripts

--- travis-script/install-dependencies.sh
+++ travis-script/install-dependencies.sh
@@ -4,7 +4,7 @@ set -ev
 
 # Install Opaml
 wget https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux -O opam
-chmod +x opam
+chmod ugo+x opam
 ./opam init --yes --comp=4.01.0 #(then say 'y' to the final question)
 eval `./opam config env`
 ./opam install --yes sawja.1.5 atdgen.1.5.0 javalib.2.3a extlib.1.5.4 #(then say 'y' to the question)
