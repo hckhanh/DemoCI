5fe0866590ed9e587e83daee8b39da3cb636379d
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-04 22:59:16 +0700

    Disable prebuild

--- travis-script/run-test-cases.sh
+++ travis-script/run-test-cases.sh
@@ -11,9 +11,9 @@ if [ "$ANDROID_APP1" == "true" ] || [ "$ANDROID_APP2" == "true" ]; then
 	cd $ANDROID_DIR/$ANDROID_PROJECT_NAME/
 
 	# Run connectedCheck of Android App 1
-	if [ "$ANDROID_APP1" == "true" ]; then
-		./gradlew :$ANDROID_APP1_MODULE_NAME:assembleDebug
-	fi
+	#if [ "$ANDROID_APP1" == "true" ]; then
+	#	./gradlew :$ANDROID_APP1_MODULE_NAME:assembleDebug
+	#fi
 
 	# Run connectedCheck of Android App 2
 	#if [ "$ANDROID_APP2" == "true" ]; then
