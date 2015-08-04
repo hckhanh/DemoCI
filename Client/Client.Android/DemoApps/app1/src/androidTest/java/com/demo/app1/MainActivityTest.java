dc0a71c283a3e8edd9ba2b811ce2695a6882a28f
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-04 20:19:54 +0700

    Add more test cases

--- Client/Client.Android/DemoApps/app1/src/androidTest/java/com/demo/app1/MainActivityTest.java
+++ Client/Client.Android/DemoApps/app1/src/androidTest/java/com/demo/app1/MainActivityTest.java
@@ -3,7 +3,9 @@ package com.demo.app1;
 import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -32,7 +34,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         btnButton = (Button) mainActivity.findViewById(R.id.button);
     }
 
-    @MediumTest
+    @SmallTest
     public void testClickToChangeButton() {
         String expectedText = "Text is changed!";
         String actualText;
@@ -42,4 +44,37 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
         assertEquals(expectedText, actualText);
     }
+
+    @MediumTest
+    public void testClickToChangeButton2() {
+        String expectedText = "Text is changed!";
+        String actualText;
+
+        TouchUtils.clickView(this, btnButton);
+        actualText = textView.getText().toString();
+
+        assertEquals(expectedText, actualText);
+    }
+
+    @LargeTest
+    public void testClickToChangeButton3() {
+        String expectedText = "Text is changed!";
+        String actualText;
+
+        TouchUtils.clickView(this, btnButton);
+        actualText = textView.getText().toString();
+
+        assertEquals(expectedText, actualText);
+    }
+
+    public void testClickToChangeButton4() {
+        String expectedText = "Text is changed!";
+        String actualText;
+
+        TouchUtils.clickView(this, btnButton);
+        actualText = textView.getText().toString();
+
+        assertEquals(expectedText, actualText);
+    }
+
 }
