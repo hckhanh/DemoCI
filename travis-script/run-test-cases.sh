9f5db304e90f1cc5a5c2adb8889000fa856daa28
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-05 02:28:41 +0700

    Final release of TravisCI structure

--- travis-script/run-test-cases.sh
+++ travis-script/run-test-cases.sh
@@ -8,20 +8,10 @@ if [ "$ANDROID_APP1" == "true" ] || [ "$ANDROID_APP2" == "true" ]; then
 	# Run infer test
 	# ./travis-script/run-infer-test.sh
 
-	cd $ANDROID_DIR/$ANDROID_PROJECT_NAME/
-
-	# Run connectedCheck of Android App 1
-	#if [ "$ANDROID_APP1" == "true" ]; then
-	#	./gradlew :$ANDROID_APP1_MODULE_NAME:assembleDebug
-	#fi
-
-	# Run connectedCheck of Android App 2
-	#if [ "$ANDROID_APP2" == "true" ]; then
-	#	./gradlew :$ANDROID_APP2_MODULE_NAME:assembleDebug
-	#fi
-
 	# Run android emulator
-	$TRAVIS_BUILD_DIR/travis-script/run-emulator.sh
+	./travis-script/run-emulator.sh
+
+	cd $ANDROID_DIR/$ANDROID_PROJECT_NAME/
 
 	# Run connectedCheck of Android App 1
 	if [ "$ANDROID_APP1" == "true" ]; then
