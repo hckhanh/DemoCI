a3f46ecba8f2e2526e2ec778bc126ec4d129941e
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-05 00:15:56 +0700

    Update test case

--- Client/Client.Android/DemoApps/app1/src/androidTest/java/com/demo/app1/MainActivityTest.java
+++ Client/Client.Android/DemoApps/app1/src/androidTest/java/com/demo/app1/MainActivityTest.java
@@ -3,7 +3,7 @@ package com.demo.app1;
 import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
-import android.test.suitebuilder.annotation.LargeTest;
+import android.test.suitebuilder.annotation.MediumTest;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,16 +25,14 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        // These lines of code bellow are very important
-        setActivityInitialTouchMode(true); // Enable touch mode in Activity tesing
-        instrumentation = getInstrumentation();
+        setActivityInitialTouchMode(true);
         mainActivity = getActivity();
 
         textView = (TextView) mainActivity.findViewById(R.id.textView);
         btnButton = (Button) mainActivity.findViewById(R.id.button);
     }
 
-    @LargeTest
+    @MediumTest
     public void testClickToChangeButton() {
         String expectedText = "Text is changed!";
         String actualText;
