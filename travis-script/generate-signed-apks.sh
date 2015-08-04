b05e0f1111f8b3dfc4c5a0ea8b6c2a6203de2948
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-04 14:19:34 +0700

    Fix generate apks script

--- travis-script/generate-signed-apks.sh
+++ travis-script/generate-signed-apks.sh
@@ -5,22 +5,22 @@ set -ev
 # Check whether this is a tag build or not
 if [ -n "$TRAVIS_TAG" ]; then
 
+	cd $ANDROID_DIR/$ANDROID_PROJECT_NAME/
+
 	# Generate release file of Android App 1
 	if [ "$ANDROID_APP1" == "true" ]; then
-		cd $ANDROID_DIR/$ANDROID_PROJECT_NAME/
 		./gradlew :$ANDROID_APP1_MODULE_NAME:assemble$BUILD_TYPE
-		cd $ANDROID_APP2_MODULE_NAME/$RELEASE_PATH_FILE/
+		cd $ANDROID_APP1_MODULE_NAME/$RELEASE_PATH_FILE/
 		mv $ANDROID_APP1_MODULE_NAME-$BUILD_TYPE.apk $ANDROID_APP1_NAME-$TRAVIS_TAG.apk
-		cd $TRAVIS_BUILD_DIR/
 	fi
 
 	# Generate release file of Android App 2
 	if [ "$ANDROID_APP2" == "true" ]; then
-		cd $ANDROID_DIR/$ANDROID_PROJECT_NAME/
 		./gradlew :$ANDROID_APP2_MODULE_NAME:assemble$BUILD_TYPE
 		cd $ANDROID_APP2_MODULE_NAME/$RELEASE_PATH_FILE/
 		mv $ANDROID_APP2_MODULE_NAME-$BUILD_TYPE.apk $ANDROID_APP2_NAME-$TRAVIS_TAG.apk
-		cd $TRAVIS_BUILD_DIR/
 	fi
 	
+	cd $TRAVIS_BUILD_DIR/
+
 fi
