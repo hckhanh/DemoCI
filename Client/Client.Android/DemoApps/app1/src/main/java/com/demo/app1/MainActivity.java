6c0a8e403540264205991632626ef6f9a5e28cc9
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-05 00:07:58 +0700

    Remove DEBUG config

--- Client/Client.Android/DemoApps/app1/src/main/java/com/demo/app1/MainActivity.java
+++ Client/Client.Android/DemoApps/app1/src/main/java/com/demo/app1/MainActivity.java
@@ -25,10 +25,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if (BuildConfig.DEBUG) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
     }
 
     @Override
