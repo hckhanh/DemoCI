dc0581038c3860d96bf29358cf6fde55577876a0
hckhanh <hoangchaukhanh93@gmail.com>
2015-10-08 11:51:41 +0700

    df

---
+++ Client/Client.Android/DemoApps/app1/src/main/java/com/demo/app1/MainActivity.java
@@ -0,0 +1,65 @@
+package com.demo.app1;
+
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnButton = (Button) findViewById(R.id.button);
+        final TextInputLayout usernameTextInputLayout = (TextInputLayout) this.findViewById(R.id.view2);
+        usernameTextInputLayout.setErrorEnabled(true);
+
+        btnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Text is changed!");
+
+                usernameTextInputLayout.setError("YOLO");
+            }
+        });
+
+        if (BuildConfig.DEBUG) {
+            getWindow().addFlags (
+                      WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                    | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                    | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                    | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+            );
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# DemoCI [![Build Status](https://travis-ci.org/hckhanh/DemoCI.svg?branch=master)](https://travis-ci.org/hckhanh/DemoCI)
+
+Demo How to make a CI repo. This is the main structure of the project.
+
+## Test plan
+
+1. Test Client
+  1. Test App1
+  2. Test App2
+  3. Test App1 + App2
+2. Test Server
+3. Test Client + Server
+  1. Test App1 + Server
+  2. Test App2 + Server
+  3. Test App1 + App2 + Server
